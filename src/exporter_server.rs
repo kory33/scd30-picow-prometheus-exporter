bc32ac89f143dcc1328782ca5c8ccb4ceb293c21
Kory | Ryosuke Kondo <korygm33@gmail.com>
2025-02-11 08:20:43 +0900

    refactor: simplify exporter_server

--- src/exporter_server.rs
+++ src/exporter_server.rs
@@ -8,12 +8,21 @@ use core2::io::Write as _;
 use embedded_io_async::Write as _;
 
 async fn write_to_socket_or_log_error(socket: &mut TcpSocket<'_>, buf: &[u8]) {
-    match socket.write_all(buf).await {
-        Ok(()) => {}
-        Err(e) => {
-            log::warn!("write error: {:?}", e);
-        }
+    if let Err(e) = socket.write_all(buf).await {
+        log::warn!("write error: {:?}", e);
     }
+}
+
+fn write_to_initial_segment_and_reborrow<'a>(
+    buf: &'a mut [u8],
+    fmt: core::fmt::Arguments<'_>,
+) -> &'a mut [u8] {
+    let mut cur = Cursor::new(&mut *buf);
+    if let Err(e) = cur.write_fmt(fmt) {
+        log::warn!("server: buffer write error: {:?}", e);
+    }
+    let position = cur.position();
+    &mut buf[..position as usize]
 }
 
 async fn serve_one_http_request(
@@ -29,13 +38,13 @@ async fn serve_one_http_request(
     let mut socket = TcpSocket::new(stack, &mut rx_buffer, &mut tx_buffer);
     socket.set_timeout(Some(Duration::from_secs(3)));
 
+    // TODO: what does gpio_set(0, _) do?
     control.gpio_set(0, false).await;
     log::info!("Listening on TCP:1234...");
     if let Err(e) = socket.accept(1234).await {
         log::warn!("accept error: {:?}", e);
         return;
     }
-
     log::info!("Received connection from {:?}", socket.remote_endpoint());
     control.gpio_set(0, true).await;
 
@@ -46,63 +55,38 @@ async fn serve_one_http_request(
 
     let response_code;
     if let Some(measurement) = measurement {
-        let mut header_buf = [0_u8; 64];
         let mut body_buf = [0_u8; 256];
+        let response_body = write_to_initial_segment_and_reborrow(&mut body_buf,
+            format_args!(
+                "cos_concentration_ppm {:.2}\r\ntemperature_deg_celsius {:.2}\r\nrelative_humidity_percent {:.2}\r\n",
+                measurement.co2_concentration, measurement.temperature, measurement.humidity
+            )
+        );
 
-        let response_body = {
-            let mut cur = Cursor::new(&mut body_buf[..]);
-            match write!(
-              &mut cur,
-              "cos_concentration_ppm {:.2}\r\ntemperature_deg_celsius {:.2}\r\nrelative_humidity_percent {:.2}\r\n",
-              measurement.co2_concentration, measurement.temperature, measurement.humidity
-          ) {
-              Ok(()) => {}
-              Err(e) => {
-                  log::warn!("write error: {:?}", e);
-                  return;
-              }
-          };
-            let position = cur.position();
-            &body_buf[..position as usize]
-        };
-
-        let response_header = {
-            let mut cur = Cursor::new(&mut header_buf[..]);
-            match write!(
-                &mut cur,
+        let mut header_buf = [0_u8; 64];
+        let response_header = write_to_initial_segment_and_reborrow(
+            &mut header_buf,
+            format_args!(
                 "HTTP/1.1 200 OK\r\nContent-Length: {}\r\n\r\n",
                 response_body.len()
-            ) {
-                Ok(_) => {}
-                Err(e) => {
-                    log::warn!("write error: {:?}", e);
-                    return;
-                }
-            };
-            let position = cur.position();
-            &header_buf[..position as usize]
-        };
+            ),
+        );
 
         write_to_socket_or_log_error(&mut socket, response_header).await;
         write_to_socket_or_log_error(&mut socket, response_body).await;
-
-        response_code = b"200";
+        response_code = 200;
     } else {
         write_to_socket_or_log_error(&mut socket, b"HTTP/1.1 503 Service Unavailable\r\n\r\n")
             .await;
-
-        response_code = b"503";
+        response_code = 503;
     }
 
-    match socket.flush().await {
-        Ok(()) => {}
-        Err(e) => {
-            log::warn!(
-                "exporter server: flush error on {:?}: {:?}",
-                response_code,
-                e
-            );
-        }
+    if let Error(e) = socket.flush().await {
+        log::warn!(
+            "exporter server: flush error on {:?}: {:?}",
+            response_code,
+            e
+        );
     };
     socket.close();
 }
