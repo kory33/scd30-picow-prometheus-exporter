c80edebf6858e4f286a731960948cf8b1899b8d4
Kory | Ryosuke Kondo <korygm33@gmail.com>
2025-02-11 07:05:05 +0900

    refactor: restructure scd30_sensor

--- src/scd30_sensor.rs
+++ src/scd30_sensor.rs
@@ -1,8 +1,8 @@
+use embassy_rp::i2c;
 use embassy_rp::i2c::{Async, I2c, Instance, InterruptHandler, SclPin, SdaPin};
 use embassy_rp::interrupt::typelevel::Binding;
-use embassy_rp::i2c;
 use embassy_rp::{peripherals::*, Peripheral};
-use embassy_sync::blocking_mutex::raw::CriticalSectionRawMutex;
+use embassy_sync::blocking_mutex::raw::{CriticalSectionRawMutex, RawMutex};
 use embassy_time::{Duration, Timer};
 use {defmt_rtt as _, panic_probe as _};
 
@@ -21,6 +21,35 @@ pub fn setup<'d, T: Instance>(
     scd30_interface::asynch::Scd30::new(i2c)
 }
 
+async fn update_measurement_once(
+    sensor: &mut scd30_interface::asynch::Scd30<impl embedded_hal_async::i2c::I2c>,
+    measurement_cell: &'static embassy_sync::mutex::Mutex<
+        impl RawMutex,
+        Option<scd30_interface::data::Measurement>,
+    >,
+) {
+    loop {
+        match sensor.is_data_ready().await {
+            Ok(scd30_interface::data::DataStatus::Ready) => break,
+            Ok(_) => {}
+            Err(e) => {
+                log::warn!("scd30: error checking data ready: {:?}", e);
+            }
+        }
+        Timer::after(Duration::from_millis(1000)).await;
+    }
+
+    match sensor.read_measurement().await {
+        Ok(measurement) => {
+            let mut guard = measurement_cell.lock().await;
+            *guard = Some(measurement);
+        }
+        Err(e) => {
+            log::warn!("scd30: error reading measurement: {:?}", e);
+        }
+    }
+}
+
 #[embassy_executor::task]
 pub async fn keep_updating_measurement(
     mut sensor: scd30_interface::asynch::Scd30<I2c<'static, I2C0, Async>>,
@@ -28,20 +57,8 @@ pub async fn keep_updating_measurement(
         CriticalSectionRawMutex,
         Option<scd30_interface::data::Measurement>,
     >,
-) {
+) -> ! {
     loop {
-        while sensor.is_data_ready().await != Ok(scd30_interface::data::DataStatus::Ready) {
-            Timer::after(Duration::from_millis(1000)).await;
-        }
-
-        match sensor.read_measurement().await {
-            Ok(measurement) => {
-                let mut guard = measurement_cell.lock().await;
-                *guard = Some(measurement);
-            }
-            Err(e) => {
-                log::warn!("error reading measurement: {:?}", e);
-            }
-        }
+        update_measurement_once(&mut sensor, measurement_cell).await;
     }
 }
